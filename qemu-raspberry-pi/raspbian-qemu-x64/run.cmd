08e78394b6469ac3b4d87456b3ac23f7fad192fb
Wonsup Yoon <pusnow@me.com>
2014-10-08 19:44:48 +0900

    smart batch finish

--- qemu-raspberry-pi/raspbian-qemu-x64/run.cmd
+++ qemu-raspberry-pi/raspbian-qemu-x64/run.cmd
@@ -1,14 +1,19 @@
+@echo off
+echo Copyright (c) 2014 SBTM Lab. http://sbtm.yonsei.ac.kr/
 PUSHD %~dp0
-if exist *.img(
+if exist *.img (
 
-for %%a in (*.img) do (
-	set imgname=%%a
-	goto break
-)
-:bresk
-cd qemu
-qemu-system-arm.exe -kernel kernel-qemu -cpu arm1176 -m 256 -M versatilepb -no-reboot -serial stdio -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" -hda ..\%a%
-)else(
-echo "img íŒŒì¼ì´ ì—†ìŠµë‹ˆë‹¤"
+	for %%a in (*.img) do (
+		set imgname=%%a
+		echo %%a ÆÄÀÏÀ» »ç¿ëÇÕ´Ï´Ù.
+		goto break
+	)
+	:break
+	cd qemu
+	qemu-system-arm.exe -kernel kernel-qemu -cpu arm1176 -m 256 -M versatilepb -no-reboot -serial stdio -append "root=/dev/sda2 panic=1 rootfstype=ext4 rw" -hda ..\%imgname%
+	cd ..
+) 
+if not exist *.img (
+echo imgÆÄÀÏÀÌ ¾ø½À´Ï´Ù.
 )
 pause
